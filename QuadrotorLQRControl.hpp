05246ea64f9fa3331a2bd8182f0b9ce4a11f9907
Javier A. Rodriguez <45160005+rodrij26@users.noreply.github.com>
2020-12-08 13:26:09 -0600

    Add files via upload

--- QuadrotorLQRControl.hpp
+++ QuadrotorLQRControl.hpp
@@ -35,203 +35,167 @@ using namespace matrix;
 class QuadrotorLQRControl
 {
 public:
-       QuadrotorLQRControl();
-       
-       void setEquilibriumPoint(Matrix<float,12,1> eqPoint);
-       
-       void setCurrentState(struct vehicle_attitude_s _v_att, struct vehicle_local_position_s  _v_local_pos);
-       
-       void setCurrentStateEkf(struct vehicle_attitude_s _v_att, struct vehicle_local_position_s  _v_local_pos);
-       
-       Matrix<float,4,1> LQRcontrol();
-       
-       void setAutoEqPointFlag(bool flag);
-    
-       void setAutoEqPoint(struct vehicle_attitude_s _v_att, struct vehicle_local_position_s  _v_local_pos); 
+	QuadrotorLQRControl();
 
-       bool getAutoEqPointFlag();
+	void setEquilibriumPoint(Matrix<float, 12, 1> eqPoint);
 
-       Matrix<float,4,1> getLQRcontrols();
-       
-       void setCurrentState(Matrix<float,12,1> _state_estimate);
+	void setCurrentState(struct vehicle_attitude_s _v_att, struct vehicle_local_position_s  _v_local_pos);
 
-       Matrix<float,4,1> normalizationControlInputs(Matrix<float,4,1> _u);
+	void setCurrentStateEkf(struct vehicle_attitude_s _v_att, struct vehicle_local_position_s  _v_local_pos);
 
-	   Matrix<float,3,1> generateRef(float time);
+	Matrix<float, 4, 1> LQRcontrol();
 
-       
+	void setAutoEqPointFlag(bool flag);
+
+	void setAutoEqPoint(struct vehicle_attitude_s _v_att, struct vehicle_local_position_s  _v_local_pos);
+
+	bool getAutoEqPointFlag();
+
+	Matrix<float, 4, 1> getLQRcontrols();
+
+	void setCurrentState(Matrix<float, 12, 1> _state_estimate);
+
+	Matrix<float, 4, 1> normalizationControlInputs(Matrix<float, 4, 1> _u);
+
+	Matrix<float, 3, 1> generateRef(float time);
+
+
 private:
-       
-      
-       
-      Matrix<float,12,1> _eq_point;
-
-      Matrix<float,4,12> _K;
-
-      Matrix<float,12,12> _P;
-      
-      Matrix<float,12,1> _current_state;
-
-      Matrix<float,12,1> _current_state_ekf;
-
-	  Matrix<float, 3, 1> _ref_points;
 
 
-      bool _auto_eq_point_flag;
-	  float ff_thrust;
-	  float ux;
-	  float uy; 
-	  float s1; 
-	  float s2;
-	  float s3; 
-	  float s4;
-	 
-	 	  
-	 
-	 // ------- reference generation -----------
 
-	  float time_ref; 
-	  float old_time_ref;
+	Matrix<float, 12, 1> _eq_point;
 
-	  float ref_x;
-	  float ref_xdot;
-	  float ref_xddot;
-	  float ref_x_old;
-	  float ref_xdot_old;
+	Matrix<float, 4, 12> _K;
 
-	  float ref_y;
-	  float ref_ydot;
-	  float ref_yddot;
-	  float ref_y_old;
-	  float ref_ydot_old;
+	Matrix<float, 12, 12> _P;
 
-	  float ref_z;
-	  float ref_zdot;
-	  float ref_zddot;
-	  float ref_z_old;
-	  float ref_zdot_old;
+	Matrix<float, 12, 1> _current_state;
 
-	 // --------- Sliding Mode Control Variables --------
-      float sx;
-	  float sy;
-	
-      float xd7;//roll 
-	  float xd7_dot;
-	  float xd7_ddot;
-	  float xd7_old;
-	  float xd7_dot_old;
-	 
-	  float xd8; //pitch 
-	  float xd8_dot;
-      float xd8_ddot;
-	  float xd8_old;
-	  float xd8_dot_old;
+	Matrix<float, 12, 1> _current_state_ekf;
 
-	  float xd9; //yaw 
-	  float xd9_dot;
-	  float xd9_ddot;
-	  float xd9_old;
-	  float xd9_dot_old;
-	 
-	 float b1;
-	 float b2;
-	 float b3;
-	 
-	 float a1;
-	 float a2;
-	 float a3; 
-	 
-	 // Quadcopter Properties
-	 float m = 0.8;
-	 float Jx = 0.005;
-	 float Jy = 0.005;
-	 float Jz = 0.009;
-	 float g = 9.81;
-	 float l = 1; //0.33 / 2;
-
-	 
-	//// For Altitude Z
-	// float c1 = 68;
-	// float k1 = 25;
-	// float k2 = 10;
-
-	// // For X-axis
-	// float c2 = 0.001;
-	// float k3 = 0.001;
-	// float k4 = 0.001;
-
-	// // For Y-axis
-	// float c3 = 0.001;
-	// float k5 = 0.001;
-	// float k6 = 0.001;
-
-	// // For phi - u2
-	// float c4 = 5; //20
-	// float k7 = 50; //5
-	// float k8 = 100; //5
-
-	//// For theta - u3
-	// float c5 = 5;
-	// float k9 = 100;
-	// float k10 = 10;
-
-	// // For psi - u4
-	// float c6 = 5; //20
-	// float k11 = 100; //5
-	// float k12 = 10; //5
-
-		// For Altitude Z
-	 float c1 = 35;
-	 float k1 = 8;
-	 float k2 = 8;
-
-	 // For X-axis
-	 float c2 = 1;//2;
-	 float k3 = 0.1;//0.1;
-	 float k4 = 0.1; //0.1/0.3;
-
-	 // For Y-axis
-	 float c3 = 1;//2;
-	 float k5 = 0.1;//0.1;
-	 float k6 = 0.1;//0.1/0.3;
-
-	 // For phi - u2
-	 float c4 = 0.5;
-	 float k7 = 0.1;
-	 float k8 = 20;
-
-	 // For theta - u3
-	 float c5 = 0.5;
-	 float k9 = 0.1;
-	 float k10 = 20;
-
-	 // For psi - u4
-	 float c6 = 0.5;
-	 float k11 = 0.5;
-	 float k12 = 0.5;
+	Matrix<float, 3, 1> _ref_points;
 
 
-	   
+	bool _auto_eq_point_flag;
 
-      void writeStateOnFile(const char *filename, Matrix <float, 12, 1> vect, hrt_abstime t); 
+	float ff_thrust;
+	float ux;
+	float uy;
+	float s1;
+	float s2;
+	float s3;
+	float s4;
 
-      Matrix <float, 4, 12> readMatrixK(const char *filename);
 
-      void writeInputOnFile(const char *filename, Matrix <float, 4, 1> vect, hrt_abstime t); 
+	// ------- reference generation -----------
 
-      void writeLyapunovOnFile(const char *filename, float value, hrt_abstime t);
+	float time_ref;
+	float old_time_ref;
 
-      Matrix <float, 12, 12> readMatrixP(const char *filename);
+	float ref_x;
+	float ref_xdot;
+	float ref_xddot;
+	float ref_x_old;
+	float ref_xdot_old;
 
-      float _past_time;
+	float ref_y;
+	float ref_ydot;
+	float ref_yddot;
+	float ref_y_old;
+	float ref_ydot_old;
 
-      Matrix<float,4,1> u_control;
-	  
-	 int sign(double v);
+	float ref_z;
+	float ref_zdot;
+	float ref_zddot;
+	float ref_z_old;
+	float ref_zdot_old;
 
-	 float sat(float s);
+	// --------- Sliding Mode Control Variables --------
 
-	
-      
+	float sx;
+	float sy;
+
+	float xd7;
+	float xd7_dot;
+	float xd7_ddot;
+	float xd7_old;
+	float xd7_dot_old;
+
+	float xd8;
+	float xd8_dot;
+	float xd8_ddot;
+	float xd8_old;
+	float xd8_dot_old;
+
+	float xd9;
+	float xd9_dot;
+	float xd9_ddot;
+	float xd9_old;
+	float xd9_dot_old;
+
+	float a1;
+	float a2;
+	float a3;
+	float b1;
+	float b2;
+	float b3;
+
+	float e = 0.1;
+	float m = 0.6;
+	float Ix = 0.0092;
+	float Iy = 0.0092;
+	float Iz = 0.0101;
+	float g = 9.81;
+
+	// For Altitude Z
+	float c1 = 35;
+	float k1 = 8;
+	float k2 = 8;
+
+	// For X-axis
+	float c2 = 1;//2;
+	float k3 = 0.1;//0.1;
+	float k4 = 0.1; //0.1/0.3;
+
+	// For Y-axis
+	float c3 = 1;//2;
+	float k5 = 0.1;//0.1;
+	float k6 = 0.1;//0.1/0.3;
+
+	// For phi - u2
+	float c4 = 0.5;
+	float k7 = 0.1;
+	float k8 = 20;
+
+	// For theta - u3
+	float c5 = 0.5;
+	float k9 = 0.1;
+	float k10 = 20;
+
+	// For psi - u4
+	float c6 = 0.5;
+	float k11 = 0.5;
+	float k12 = 0.5;
+
+
+	void writeStateOnFile(const char* filename, Matrix <float, 12, 1> vect, hrt_abstime t);
+
+	Matrix <float, 4, 12> readMatrixK(const char* filename);
+
+	void writeInputOnFile(const char* filename, Matrix <float, 4, 1> vect, hrt_abstime t);
+
+	void writeLyapunovOnFile(const char* filename, float value, hrt_abstime t);
+
+	Matrix <float, 12, 12> readMatrixP(const char* filename);
+
+	float _past_time;
+
+	Matrix<float, 4, 1> u_control;
+
+	int sign(double v);
+
+	float sat(float s);
+
 
 };
