290aa2e6f894016e5f1995294dfd5fdd49348156
Siddartha Idukuda <70922223+sidukuda@users.noreply.github.com>
2020-12-12 20:02:16 -0500

    PX4 IMPLEMENTATION FILES

--- QuadrotorLQRControl.cpp
+++ QuadrotorLQRControl.cpp
@@ -30,12 +30,6 @@ QuadrotorLQRControl::QuadrotorLQRControl()
        _current_state(i,0) = 0.0f;
        _eq_point(i,0) = 0.0f;
     }
-/* 
-      _eq_point(1,0) =  -1.0f; //x
-      _eq_point(3,0) =  -1.0f; //y
-      _eq_point(5,0) = -1.0f; //z */
-	
-	 // Sliding Mode Control 
 
 	
 	 ux = 0.0f;
@@ -66,12 +60,6 @@ QuadrotorLQRControl::QuadrotorLQRControl()
      u_control(2,0) = 0.0f;
      u_control(3,0) = 0.0f;
 
-     //_K = readMatrixK("/home/raffaele/PX4/Firmware/src/modules/mc_att_control/lqr_files/new_controller.txt");
-	 //_K = readMatrixK("C:\PX4\home\px4_external\src\modules\mc_att_control\lqr_files\new_controller.txt");
-     //_P = readMatrixP("/home/raffaele/PX4/Firmware/src/modules/mc_att_control/lqr_files/new_pe.txt");
-	 //_P = readMatrixP("C:\PX4\home\px4_external\src\modules\mc_att_control\lqr_files\new_pe.txt");
-
-     // ff_thrust = 5.886f; // [N]
 
      _auto_eq_point_flag = true;
 
@@ -152,18 +140,9 @@ Matrix<float,4,1> QuadrotorLQRControl::LQRcontrol()
 	  _eq_point(0,0) =  1*sin(time_ref); //xdot
       _eq_point(3,0) = -1*sin(1*time_ref); //y
 	  _eq_point(2,0) = -1*cos(time_ref); //ydot
-/* 	 _eq_point(1,0) = -_current_time/2.5; //x
-     _eq_point(3,0) = sin(_current_time); //y
-     _eq_point(5,0) = sin(_current_time); //z  */
 	 time_ref = time_ref + dt; 
 	 
 	 //-------- Sliding Mode Control -------
-/* 	 _eq_point(0,0) = (-4.0f*pow((_current_time),3))*exp(-pow((_current_time),4)); //xdot
-	 _eq_point(1,0) = -1.0f + exp(-pow((_current_time),4)); //x
-	 _eq_point(2,0) = (-4.0f*pow((_current_time),3))*exp(-pow((_current_time),4)); //ydot
-     _eq_point(3,0) = -1.0f + exp(-pow((_current_time),4)); //y
-	 _eq_point(4,0) = (-4.0f*pow((_current_time),3))*exp(-pow((_current_time),4)); //zdot
-     _eq_point(5,0) = -1.0f + exp(-pow((_current_time),4)); //z  */
 	 
 	 xd7_dot = (xd7 - xd7_old)/dt; 
 	 xd8_dot = (xd8 - xd8_old)/dt;
@@ -182,109 +161,9 @@ Matrix<float,4,1> QuadrotorLQRControl::LQRcontrol()
      
      _past_time = _current_time;
 
-     //delta_x   = _current_state - _eq_point;    
-     //u_control = - _K*(delta_x); 
- 
-     //delta_x_T = delta_x.transpose();
-    
-     //v_b = delta_x_T*_P;
-     //_lyap_fun = v_b*delta_x;
-     //cout<< dt << "\t" << _P(0,0) << "\n";
-     // !! IMPORTANT scale the control inputs.......
    
      //------------------------ Sliding Mode Control ---------------------------------------
    
-      
-/* 	   float m = 0.8;
-	   float Ix = 0.005;
-	   float Iy = 0.005;
-	   float Iz = 0.005;
-	   float g = 9.8;
-   
-	   // For Altitude Z
-	   float c1 = 35;
-	   float k1 = 8;
-	   float k2 = 8;
-	   
-	   // For X-axis
-	   float c2 = 1;
-	   float k3 = 0.1;
-	   float k4 = 0.1;
-	   
-	   // For Y-axis
-	   float c3 = 1;
-	   float k5 = 0.1;
-	   float k6 = 0.1;
-	   
-	   // For phi - u2
-	   float c4 = 5;
-	   float k7 = 5;
-	   float k8 = 5;
-	   
-	   // For theta - u3
-	   float c5 = 5;
-	   float k9 = 5;
-	   float k10 = 5;
-	   
-	   // For psi - u4
-	   float c6 = 2;
-	   float k11 = 0.5;	   */
-	   
-	   //Matlab State Order  ---------- 
-	   // x(1) - pos_x    		| _current_state(1,0)
-	   // x2 - pos_y    		| _current_state(3,0)
-	   // x3 - pos_z    		| _current_state(5,0)
-	   // x4 - x_dot 			| _current_state(0,0)
-	   // x5 - y_dot			| _current_state(2,0)
-	   // x6 - z_dot			| _current_state(4,0)
-	   // x7 - roll-phi 		| _current_state(7,0)
-	   // x8 - pitch-theta 		| _current_state(9,0)
-	   // x9 - yaw - psi		| _current_state(11,0)
-	   // x10 - roll_dot		| _current_state(6,0)
-	   // x11 - pitch_dot		| _current_state(8,0)
-	   // x12 - psi_dot 		| _current_state(10,0)
-	 
-
-	   /* s1 = (_eq_point(4,0) - _current_state(4,0)) + c1*(_eq_point(5,0)-_current_state(5,0));
-	   //sx = (_eq_point(0,0) - _current_state(0,0)) + c2*(_eq_point(1,0)-_current_state(1,0));
-	   //sy = (_eq_point(2,0) - _current_state(2,0)) + c3*(_eq_point(3,0)-_current_state(3,0));
-	   //u_control(0,0) = -(m/(cos(x(7))*cos(x(8))))*(k1*sign(s1)+k2*s1+g+c1*(r(6)-x(6)));
-	   //sx = c2*(_eq_point(1,0)-_current_state(1,0));
-	   //sy = c3*(_eq_point(3,0)-_current_state(3,0));
-	   
-	   u_control(0,0) = -(m/(cos(_current_state(7,0))*cos(_current_state(9,0))))*(k1*sign(s1)+k2*s1+g+c1*(_eq_point(4,0)-_current_state(4,0)));
-	   
-	   //motion control about x and y axis
-/* 	   ux = (m/u_control(0,0))*(k3*sign(sx) + k4*sx + c2*(_eq_point(0,0) -_current_state(0,0)));
-	   uy = (m/u_control(0,0)*(k5*sign(sy) + k6*sy + c3*(_eq_point(2,0)-_current_state(2,0)))); */
-	   
-	   /* ux = (m/u_control(0,0))*(k3*sat(sx) + k4*sx + c2*(_eq_point(0,0) -_current_state(0,0)));
-	   uy = (m/u_control(0,0))*(k5*sat(sy) + k6*sy + c3*(_eq_point(2,0)-_current_state(2,0))); */
-	   //ux = sat(ux);
-	   //uy = sat(uy);
-	   
-	   //xd7 = (asin(uy-floor(uy))); //+ M_PI*floor(uy)/2; // desired as functions 
-	   //xd8 = (asin(-ux/cos(xd7) - floor(-ux/cos(xd7))));//+ M_PI*floor(-ux/cos(xd7))/2;
-/* 	   xd7 = (asin(ux-floor(ux))) + M_PI*floor(ux)/2; // desired as functions 
-	   xd8 = (asin(-uy/cos(xd7) - floor(-uy/cos(xd7))))+ M_PI*floor(-uy/cos(xd7))/2; */
-	   /* xd7 = asin((ux*sin(_current_state(11,0))) - (uy*cos(_current_state(11,0))));
-	   xd8 = asin(((ux*cos(xd9)) - (uy*sin(xd9)))/cos(xd7));
-	   xd9 = atan((_eq_point(2,0)-_current_state(2,0))/(_eq_point(0,0)-_current_state(0,0))); */
-	   
-	   
-	   //angular motion control 
-	   /* s2 = (xd7_dot-_current_state(6,0)) + c4*(xd7-_current_state(7,0));
-	   s3 = (xd8_dot-_current_state(8,0)) + c5*(xd8-_current_state(9,0));
-	   s4 = (xd9_dot-_current_state(10,0)) + c6*(xd9-_current_state(11,0)); */
-	   
-	   //
-/* 	   u_control(1,0) = Ix*(k8*sign(s2)+k9*s2 + xd7_ddot + c4*(xd7_dot - _current_state(6,0)));
-	   u_control(2,0) = Iy*(k9*sign(s3)+k10*s3 + xd8_ddot + c5*(xd8_dot - _current_state(8,0)));
-	   u_control(3,0) = Iz*(k11*sign(s4)+k12*s4 + c6*(0 -_current_state(10,0))); */
-	   
-	   /* u_control(1,0) = Ix*(k8*sat(s2)+k9*s2 + xd7_ddot + c4*(xd7_dot - _current_state(6,0)));
-	   u_control(2,0) = Iy*(k9*sat(s3)+k10*s3 + xd8_ddot + c5*(xd8_dot - _current_state(8,0)));
-	   u_control(3,0) = Iz*(k11*sat(s4)+k12*s4 + c6*(xd9_dot -_current_state(10,0))); */
 	   a1 = (Ix-Iy)/Ix;
 	   a2 = (Iz-Ix)/Iy;
 	   a3 = (Ix-Iy)/Iz;
@@ -296,65 +175,30 @@ Matrix<float,4,1> QuadrotorLQRControl::LQRcontrol()
        
        sx = (_eq_point(0,0) - _current_state(0,0)) + c2*(_eq_point(1,0)-_current_state(1,0));
 	   sy = (_eq_point(4,0) - _current_state(2,0)) + c3*(_eq_point(3,0)-_current_state(3,0));
-	   //cout << "s1: " << s1 << ", sx: " << sx << ", sy: " << sy << "\n";
-	   //u_control(0,0) = -(m/(cos(x(7))*cos(x(8))))*(k1*sign(s1)+k2*s1+g+c1*(r(6)-x(6)));
 
 	   
 	   //motion control about x and y axis
 	   u_control(0,0) = -(m/(cos(_current_state(7,0)) * cos(_current_state(9, 0)))) * (k1*sat(s1) + k2*s1 + g + c1 * (_eq_point(4, 0) - _current_state(4, 0)));
 	   u1 = u_control(0,0);
-	   //u_control(0,0) = -((m/(cos(_current_state(7,0)) * cos(_current_state(9, 0)))) * (g + c1 * (_eq_point(4, 0) - _current_state(4, 0))));
        ux = (m/u_control(0,0))*(k3*sat(sx) + k4*sx + c2*(_eq_point(0,0) -_current_state(0,0)));
        uy = (m/u_control(0,0))*(k5*sat(sy) + k6*sy + c3*(_eq_point(2,0)-_current_state(2,0)));
-	   //ux = ((m/u_control(0,0))*(c2*(_eq_point(0,0) -_current_state(0,0))))*sat(sx/0.5);
-       //uy = ((m/u_control(0,0))*(c3*(_eq_point(2,0)-_current_state(2,0))))*sat(sy/0.5);
 	   
+	  //Utilizing Norm
 	   u_n = pow(pow(u1,2)+pow(ux,2)+pow(uy,2),0.5);
 	   ux_n = ux/u_n;
 	   uy_n = uy/u_n;
-             
-       //xd7 = asin(sat(ux * sin(_current_state(11, 0)) - uy * cos(_current_state(11, 0))));
 	   xd7 = asin(sat(uy_n));
 	   xd8 = asin(sat(-ux_n/cos(xd7)));
-	   //xd8 = asin(sat(-ux/cos(_current_state(7,0))));
-       //xd8 = asin(sat((ux * cos(xd9) - uy * sin(xd9)) / (cos(xd7))));
-       //xd9 = atan((_eq_point(3, 0) - _current_state(3, 0)) / (_eq_point(1, 0) - _current_state(1, 0)));
-/* 	   xd7 = (asin(sat(ux))); // desired as functions 
-	   xd8 = (asin(sat(-uy/cos(xd7)))); */
-       //xd9 = sat(atan2((_eq_point(3, 0) - _current_state(3, 0)), (_eq_point(1, 0) - _current_state(1, 0))));
-	   //xd9 = atan2((_eq_point(1, 0) - _current_state(1, 0)), (_eq_point(3, 0) - _current_state(3, 0)));
 	   xd9 = 0.0;
-
-       //cout << "xd7: " << xd7 << ", xd8: " << xd8 << ", xd9: " << xd9 << "\n";
-       //cout << "xd7_dot: " << xd7_dot << ", xd8_dot: " << xd8_dot << ", xd9_dot: " << xd9_dot << "\n";
-       //cout << "xd7_ddot: " << xd7_ddot  << ", xd8_ddot: " << xd8_ddot << ", xd9_ddot: " << xd9_ddot  << "\n";
-       //cout << "------------------------------------------------------------------- " << "\n \n";
-    
-
-       //ux = sat(ux);
-       //uy = sat(uy);
-
-       //xd7 = (asin(uy)); //+ m_pi*floor(uy)/2; // desired as functions 
-       //xd8 = (asin(-ux / cos(xd7))); //+ m_pi*floor(-ux/cos(xd7))/2;
-
-       //cout << "xd7: " << xd7 << "\n" ;
 	   
 	   //angular motion control --- s = e_dot + c*e
        s2 = (xd7_dot-_current_state(6,0)) + c4*(xd7-_current_state(7,0));
        s3 = (xd8_dot-_current_state(8,0)) + c5*(xd8-_current_state(9,0));
        s4 = (xd9_dot-_current_state(10,0)) + c6*(xd9-_current_state(11,0));
        
-
-	   //cout << "s2: " << s2 << ", s3: " << s3 << ", s4: " << s4 << "\n";
-	
-
-       //u_control(0,0) = -(m/(cos(_current_state(7,0)) * cos(_current_state(9, 0)))) * (k1*sat(s1) + k2*s1 + g + c1 * (_eq_point(4, 0) - _current_state(4, 0)));
    	   u_control(1,0) = (1/b1)*(k7*sat(s2) + k8*s2 + xd7_ddot - a1*_current_state(8,0)*_current_state(10,0) + c4*(xd7_dot - _current_state(6,0))); //roll
-	   //u_control(1,0) = ((1/b1)*(xd7_ddot - a1*_current_state(8,0)*_current_state(10,0) + c4*(xd7_dot - _current_state(6,0))))*sat(s2/0.001);
 	   u_control(2,0) = (1/b2)*(k9*sat(s3) + k10*s3 + xd8_ddot - a2*_current_state(6,0)*_current_state(10,0) + c5*(xd8_dot - _current_state(8,0))); //pitch
-	   //u_control(2,0) = ((1/b2)*(xd8_ddot - a2*_current_state(6,0)*_current_state(10,0) + c5*(xd8_dot - _current_state(8,0))))*sat(s3/0.001);
 	   u_control(3,0) = (1/b3)*(k11*sat(s4) + k12*s4 + xd9_ddot - a3*_current_state(6,0)*_current_state(8,0) + c6*(xd9_dot -_current_state(10,0))); //yaw
-	   //u_control(3,0) = ((1/b3)*(xd9_ddot - a3*_current_state(6,0)*_current_state(9,0) + c6*(xd9_dot -_current_state(10,0))))*sat(s4/0.001);
 
 
 	 
